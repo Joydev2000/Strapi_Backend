0f185ec5e6ca8222eb63c01b4a60c7e1571c5bb1
DevSwarasacademy <137753258+Joydev2000@users.noreply.github.com>
2025-06-22 22:19:57 +0530

    fdd

--- config/middlewares.js
+++ config/middlewares.js
@@ -1,8 +1,25 @@
 module.exports = [
   'strapi::logger',
   'strapi::errors',
-  'strapi::security',
-  'strapi::cors',
+  {
+    name: 'strapi::security',
+    config: {
+      contentSecurityPolicy: {
+        directives: {
+          'connect-src': ["'self'", 'https:'],
+        },
+      },
+    },
+  },
+  {
+    name: 'strapi::cors',
+    config: {
+      origin: ['https://strapifrontend-production.up.railway.app/'], // 👈 YOUR LIVE NEXT.JS DOMAIN
+      methods: ['GET', 'POST', 'PUT', 'DELETE'],
+      headers: ['Content-Type', 'Authorization'],
+      credentials: true,
+    },
+  },
   'strapi::poweredBy',
   'strapi::query',
   'strapi::body',
